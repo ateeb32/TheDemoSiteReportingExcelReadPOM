282e68cbb5dfa012f5a7155d214ff7a306e3b18e
ateeb32 <l.ateeb@hotmail.com>
2018-09-19 12:58:48 +0100

    Wed 1

---
+++ src/test/java/com/qa/ateeb/TheDemoSiteReportingExcelPOM/TestSuite.java
@@ -0,0 +1,13 @@
+package com.qa.ateeb.TheDemoSiteReportingExcelPOM;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestDemoSite.class
+})
+
+public class TestSuite {
+
+}
